4cc9d51ee27a87959275695e83dbe51811161898
AleroMcKey <108853104+AleroMcKey@users.noreply.github.com>
2025-07-16 08:38:40 +1000

    Update updater.py

--- src/sql/updater.py
+++ src/sql/updater.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 
 def load_sql_data(connection_string, query):
     sql_string = "sql"
-    onnection_string = "connection string"
+    connection_string = "connection string"
     engine = create_engine(connection_string)
     with engine.connect() as conn:
         df = pd.read_sql_query(query, conn)
